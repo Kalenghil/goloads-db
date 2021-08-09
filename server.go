d1567502c4b40a733da08f3907f4c0695e902cc1
Kalenghil <37372132+Kalenghil@users.noreply.github.com>
2021-08-09 12:41:10 +0300

    delete https

--- server.go
+++ server.go
@@ -240,5 +240,5 @@ func main() {
 	mux.Handle("/favicon.ico", http.HandlerFunc(AdsServer.sendFaviconHandler))
 	mux.Handle("/add", http.HandlerFunc(AdsServer.receivePostHandler))
 	mux.Handle("/analytics", http.HandlerFunc(AdsServer.sendAnalyticsHandler))
-	log.Fatal(http.ListenAndServe("https://goloads-db.herokuapp.com:8080", mux))
+	log.Fatal(http.ListenAndServe("goloads-db.herokuapp.com:8080", mux))
 }
